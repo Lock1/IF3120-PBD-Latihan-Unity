0c995053ebcb3f86aca647c8f3a451c5862e7d34
Lock1 <30568743+Lock1@users.noreply.github.com>
2022-03-24 13:29:09 +0700

    Penambahan kamera dan movement
    Penambahan kamera dan movement player

--- LatihanPBD/Assets/Scripts/Player/PlayerMovement.cs
+++ LatihanPBD/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,48 @@
 ﻿using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    
+public class PlayerMovement : MonoBehaviour {
+    public float speed = 6f;
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidbody;
+    int floorMask;
+    float camRayLength = 100f;
+
+    private void Awake() {
+        floorMask       = LayerMask.GetMask("Floor");
+        anim            = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical   = Input.GetAxisRaw("Vertical");
+        Move(horizontal, vertical);
+        Turning();
+        Animation(horizontal, vertical);
+    }
+
+
+    void Animation(float h, float v) {
+        anim.SetBool("IsWalking", h != 0f || v != 0f);
+    }
+
+    void Move(float h, float v) {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Turning() {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit floorHit;
+
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask)) {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y       = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
 }
